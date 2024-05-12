ad7742770c43af5f994f844d42ceeeb4be33df50
Idan Bohuslavskyi <idan.bohuslavskyi@bankleumi.co.il>
2024-05-12 08:00:41 +0300

    .

--- src/sw.ts
+++ src/sw.ts
@@ -1,23 +1,5 @@
 import { Serwist } from "serwist";
 
-// Listen for the 'install' event
-self.addEventListener('install', () => {
-  console.log('Service worker installing...');
-  // Perform install steps
-});
-
-// Listen for the 'activate' event
-self.addEventListener('activate', () => {
-  console.log('Service worker activating...');
-  // Perform activation steps
-});
-
-// Listen for the 'fetch' event
-self.addEventListener('fetch', event => {
-  console.log('Handling fetch event for', event?.request?.url);
-  // Respond to fetch event
-});
-
 const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
   skipWaiting: true,
