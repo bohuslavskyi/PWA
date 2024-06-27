6590c92b89ee34c9f8a6e4be7dd18935cf92291a
Idan Bohuslavskyi <idan.bohuslavskyi@bankleumi.co.il>
2024-06-27 07:12:59 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
