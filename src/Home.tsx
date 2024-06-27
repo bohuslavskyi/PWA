47407bef0969d03caa18f3bfa290485776daa73b
Idan Bohuslavskyi <idan.bohuslavskyi@bankleumi.co.il>
2024-06-27 07:19:14 +0300

    .

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const navigate = useNavigate();
+
   return (
     <div>
       <h2>Home Page</h2>
       <p>Welcome to the Home page!</p>
+      <button onClick={() => navigate('/')}>Log Out</button>
     </div>
   );
 }
